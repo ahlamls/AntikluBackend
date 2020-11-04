8a65182b6e7f080456d062ef2f4d80dd39cbd1af
ahlamls <fulldoang@gmail.com>
2020-11-04 23:34:31 +0700

    gak tau belum ke push

---
+++ ngehack-mandul-7-turunan/addmenu.php
@@ -0,0 +1,3 @@
+<?php
+//INSERT INTO `user_cart` (`id`, `waktu`, `user_id`, `resto_id`, `menu_id`, `quantity`, `info`) VALUES (NULL, current_timestamp(), '9', '2', '4', '2', 'meth nya yang warna biru ');
+ ?>
